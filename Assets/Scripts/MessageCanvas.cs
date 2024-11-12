5f116a98534b6095a9c2912880f90a4a25d6143a
GeorgGrech <fgegr001@edu.xamk.fi>
2024-11-12 16:42:39 +0100

    Dynamic UI Scale

--- Assets/Scripts/MessageCanvas.cs
+++ Assets/Scripts/MessageCanvas.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class MessageCanvas : MonoBehaviour
 {
     // Update is called once per frame
-    [SerializeField] float globalScale;
+    [SerializeField] float scaleResize;
     void Update()
     {
         if (gameObject.activeSelf) // Only update if should be visible to player
         {
             transform.rotation = Camera.main.transform.rotation;
+
+            float dynamicScale = Vector3.Distance(transform.position, Camera.main.transform.position) * scaleResize;
+            transform.localScale = new Vector3(dynamicScale, dynamicScale, 1);
         }
     }
 }
